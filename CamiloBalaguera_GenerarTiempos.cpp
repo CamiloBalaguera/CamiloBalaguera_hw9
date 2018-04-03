c6c10cb14f142b9df42c17ed9172eccd16c113c8
Camilo Balaguera <ca.balaguera@uniandes.edu.co>
2018-04-03 16:17:45 -0500

    Se crearon las funciones get_time y fibonacci. Además se creó la forma en el main para imprimir el tiempo que se demora en calcular el N-esimo numero de fibonacci

--- CamiloBalaguera_GenerarTiempos.cpp
+++ CamiloBalaguera_GenerarTiempos.cpp
@@ -1,1 +1,43 @@
-ls
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int fibonacci(int N);
+float get_time(int N);
+int main()
+{
+	for(int i = 0; i<35 ; i++)
+	{
+		float t = get_time(i);
+		cout << "Tiempo: " << t <<" " << "Fibonacci: " << i << endl;
+	}	
+	return 0;
+}
+
+
+int fibonacci(int N)
+	{
+		int fib = 0;
+		if (N == 0)
+			{
+				return 0;
+			}
+		if (N == 1)
+			{
+				return 1;
+			}
+		else
+			{
+				return fibonacci(N-1) + fibonacci(N-2);
+			}
+	}
+
+float get_time(int N)
+	{
+		clock_t t;
+		t = clock();
+		fibonacci(N);
+		t = clock() - t;
+		float time = ((float)t)/CLOCKS_PER_SEC;
+		return time;
+	}
